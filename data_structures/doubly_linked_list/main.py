40dccf8d4888595255ef65c8c4da59c253b4e057
David Garcia <david_5043@hotmail.com>
2024-05-02 21:12:18 -0600

    fixed linked lists

--- data_structures/doubly_linked_list/main.py
+++ data_structures/doubly_linked_list/main.py
@@ -1,24 +1,26 @@
-"""Singly Linked List"""
+"""Doubly Linked List"""
 
 
 class Node:
-    """list node"""
+    """List node"""
 
     def __init__(self, value) -> None:
         self.next = None
+        self.prev = None
         self.value = value
 
 
-class LinkedList:
-    """Linked list"""
+class DoublyLinkedList:
+    """Doubly Linked List"""
 
     def __init__(self, value) -> None:
-        new_node = Node(value)
-        self.head = new_node
-        self.tail = new_node
+        """Constructor"""
+        aux = Node(value)
+        self.head = aux
+        self.tail = aux
 
     def append(self, value):
-        """add a new node at the end of the list"""
+        """adds a new node at the beginning of the list"""
         aux = Node(value)
 
         if self.head is None:
@@ -29,7 +31,7 @@ class LinkedList:
             self.tail = aux
 
     def prepend(self, value):
-        """add a new node at the beginning of the list"""
+        """adds a new node at the end of the list"""
         aux = Node(value)
 
         if self.head is None:
@@ -42,12 +44,13 @@ class LinkedList:
     def print(self):
         """prints the list"""
         curr = self.head
+
         while curr:
             print(curr.value)
             curr = curr.next
 
     def pop_end(self):
-        """deletes a node at the end"""
+        """deletes a node at the end of the list"""
         if self.head is None:
             return
 
@@ -55,33 +58,33 @@ class LinkedList:
         while curr.next.next is not None:
             curr = curr.next
 
-        curr.next = None
         self.tail = curr
+        curr.next = None
 
     def pop_first(self):
-        """deletes a node at the begining"""
+        """deletes a node at the beginning of the list"""
         if self.head is None:
             return
 
         self.head = self.head.next
 
 
-print("List:")
-my_list = LinkedList(0)
+print("Doubly linked list")
+my_list = DoublyLinkedList(0)
 my_list.append(1)
 my_list.append(2)
 my_list.append(3)
 my_list.append(4)
-my_list.append(5)
-my_list.prepend(6)
 my_list.print()
-print()
 
-print("Pop first")
-my_list.pop_first()
+print("\nprepend")
+my_list.prepend(7)
 my_list.print()
-print()
 
-print("Pop end")
+print("\nPop end")
 my_list.pop_end()
+my_list.print()
+
+print("\nPop first")
+my_list.pop_first()
 my_list.print()
