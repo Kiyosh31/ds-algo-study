293729f8857cb5e4987d5a9b02bdb9a3d055dc25
David Garcia <david_5043@hotmail.com>
2024-05-14 18:46:25 -0600

    fixed sorts

--- algorithms/sorting/merge_sort/merge_sort.py
+++ algorithms/sorting/merge_sort/merge_sort.py
@@ -1,55 +1,59 @@
 """Merge sort"""
 
 
-def merge_sort(arr, start, end):
-    """returns an ordered array"""
-    if end - start + 1 <= 1:
+# Implementation of MergeSort
+def merge_sort(arr, s, e):
+    """orders arr"""
+    if e - s + 1 <= 1:
         return arr
 
-    # The middle index of array
-    middle = (start + end)//2
+    # The middle index of the array
+    m = (s + e) // 2
 
     # Sort the left half
-    merge_sort(arr, start, end)
+    merge_sort(arr, s, m)
 
     # Sort the right half
-    merge_sort(arr, middle + 1, end)
+    merge_sort(arr, m + 1, e)
 
     # Merge sorted halfs
-    merge(arr, start, middle, end)
+    merge(arr, s, m, e)
 
     return arr
 
+# Merge in-place
 
-def merge(arr, start, middle, end):
-    """merge left and right"""
-    left = arr[start: middle + 1]
-    right = arr[middle + 1: end + 1]
 
-    i = 0     # index of left
-    j = 0     # index for right
-    k = start  # index of arr
+def merge(arr, s, m, e):
+    """here"""
+    # Copy the sorted left & right halfs to temp arrays
+    L = arr[s: m + 1]
+    R = arr[m + 1: e + 1]
+
+    i = 0  # index for L
+    j = 0  # index for R
+    k = s  # index for arr
 
     # Merge the two sorted halfs into the original array
-    while i < len(arr) and j < len(right):
-        if left[i] <= right[j]:
-            arr[k] = left[i]
+    while i < len(L) and j < len(R):
+        if L[i] <= R[j]:
+            arr[k] = L[i]
             i += 1
         else:
-            arr[k] = right[j]
+            arr[k] = R[j]
             j += 1
         k += 1
 
     # One of the halfs will have elements remaining
-    while i < len(left):
-        arr[k] = left[i]
+    while i < len(L):
+        arr[k] = L[i]
         i += 1
         k += 1
-    while j < len(right):
-        arr[k] = right[j]
+    while j < len(R):
+        arr[k] = R[j]
         j += 1
         k += 1
 
 
-example = [3, 2, 4, 1, 6]
-print(merge_sort(example, example[0], len(example)))
+example = [3, 2, 1, 4, 6]
+print(merge_sort(example, 0, len(example)-1))
