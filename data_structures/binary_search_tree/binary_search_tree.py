f89f7006b7ee031dfc72c09c51ac712463cc3b81
David Garcia <david_5043@hotmail.com>
2024-05-16 19:36:52 -0600

    bst

--- data_structures/binary_search_tree/binary_search_tree.py
+++ data_structures/binary_search_tree/binary_search_tree.py
@@ -1,67 +1,109 @@
 """
-Binary tree
+Binary Search Tree
 
-left = lesser
+left = lesses
 right = greater
 """
 
 
 class TreeNode:
-    """Treenode"""
+    """Tree Node"""
 
-    def __init__(self, val):
-        self.val = val
+    def __init__(self, value):
+        self.value = value
         self.left = None
         self.right = None
 
-    def __str__(self) -> str:
-        return f"value: {self.val}, left: {self.left}, right: {self.right}"
 
-
-def insert(root, val):
-    """Insert a new node and return the root of the BST."""
+def search(root, target):
+    """Search for a node in tree"""
     if not root:
-        return TreeNode(val)
+        return False
 
-    if val > root.val:
-        root.right = insert(root.right, val)
-    elif val < root.val:
-        root.left = insert(root.left, val)
+    if target > root.value:
+        return search(root.right, target)
+    elif target < root.value:
+        return search(root.left, target)
+
+    return True
+
+
+def insert(root, value):
+    """Insert a node in tree"""
+    if not root:
+        return TreeNode(value)
+
+    if value > root.value:
+        root.right = insert(root.right, value)
+    elif value < root.value:
+        root.left = insert(root.left, value)
     return root
 
 
-def min_value_node(root):
-    """Return the minimum value node of the BST."""
+def min_value(root):
+    """Return the minimum value of tree"""
     curr = root
+
     while curr and curr.left:
         curr = curr.left
+
     return curr
 
 
-def remove(root, val):
-    """Remove a node and return the root of the BST."""
+def remove(root, value):
+    """Remove a node and return the root of the tree"""
     if not root:
-        return None
+        return
 
-    if val > root.val:
-        root.right = remove(root.right, val)
-    elif val < root.val:
-        root.left = remove(root.left, val)
+    if value > root.value:
+        root.right = remove(root.right, value)
+    elif value < root.value:
+        root.left = remove(root.left, value)
     else:
         if not root.left:
             return root.right
         elif not root.right:
             return root.left
         else:
-            min_node = min_value_node(root.right)
-            root.val = min_node.val
-            root.right = remove(root.right, min_node.val)
+            min_node = min_value(root.right)
+            root.value = min_node.value
+            root.right = remove(root.right, min_node.value)
     return root
 
 
-def breadth_first_search(root):
-    """it traverse the tree by level"""
-    queue = []  # Queue
+def inorder(root):
+    """Traverse"""
+    if not root:
+        return
+
+    inorder(root.left)
+    print(root.value)
+    inorder(root.right)
+
+
+def preorder(root):
+    """Traverse"""
+    if not root:
+        return
+
+    print(root.value)
+    preorder(root.left)
+    preorder(root.right)
+
+
+def postorder(root):
+    """Traverse"""
+    if not root:
+        return
+
+    postorder(root.left)
+    postorder(root.right)
+    print(root.value)
+
+
+def bfs(root):
+    """Breadth First Search"""
+    queue = []
 
     if root:
         queue.append(root)
@@ -69,21 +111,36 @@ def breadth_first_search(root):
     level = 0
     while len(queue) > 0:
         print("level: ", level)
-        for i in range(len(queue)):
+        for _ in range(len(queue)):
             curr = queue.pop(0)
-            print(curr.val)
+            print(curr.value)
             if curr.left:
-                queue.append(curr.val)
+                queue.append(curr.left)
             if curr.right:
                 queue.append(curr.right)
-        level += i
+        level += 1
 
 
 bst = TreeNode(4)
-insert(bst, 3)
-insert(bst, 6)
-insert(bst, 2)
-insert(bst, 5)
-insert(bst, 7)
-# print(bst)
-breadth_first_search(bst)
+bst = insert(bst, 3)
+bst = insert(bst, 6)
+bst = insert(bst, 2)
+bst = insert(bst, 5)
+bst = insert(bst, 7)
+
+print("Traverse inorder")
+inorder(bst)
+
+print("Traverse preorder")
+preorder(bst)
+
+print("Traverse postoder")
+postorder(bst)
+
+print("Breadth First Search")
+bfs(bst)
+
+print("Does 3 exist in tree? ", search(bst, 3))
+print("Does 15 exist in tree? ", search(bst, 15))
+
+bst = remove(bst, 6)
