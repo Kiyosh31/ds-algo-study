306903f683a3bdb33c78491ae6ce457ebc4a5077
David Garcia <david_5043@hotmail.com>
2024-06-02 09:22:31 -0600

    graph backtracking

---
+++ algorithms/graph_traverse/dfs.py
@@ -0,0 +1,53 @@
+"""
+Dfs/Backtracking
+
+Count the unique paths from the top left to the bottom right.
+A single path may only move along 0's and can't visit same cell
+more than once.
+"""
+
+# Grid -> the matrix
+# r -> coordX
+# c -> coordY
+# visit -> flag to know if this node is visitted
+# O(n.m)
+
+
+def dfs(matrix_graph, r, c, visit):
+    """traverse a graph to find the exit path"""
+    # take out the dimensions of the matrix
+    ROWS, COLS = len(matrix_graph), len(matrix_graph[0])
+
+    # check if the actual position is out of matrix
+    # did we visitted this position or
+    # is this position blocked
+    if min(r, c) < 0 or r == ROWS or c == COLS or (r, c) in visit or grid[r][c] == 1:
+        return 0
+
+    # did we reached the destination?
+    if r == ROWS - 1 and c == COLS - 1:
+        return 1
+
+    # mark this coord as visitted
+    visit.add((r, c))
+
+    # This count is to know how many resolution paths exist on the matrix
+    count = 0
+    # we move in all directions (up, down, left, right)
+    count += dfs(matrix_graph, r + 1, c, visit)
+    count += dfs(matrix_graph, r - 1, c, visit)
+    count += dfs(matrix_graph, r, c + 1, visit)
+    count += dfs(matrix_graph, r, c - 1, visit)
+
+    # mark this coord as unvisitted
+    visit.remove((r, c))
+    return count
+
+
+matrix = [
+    [0, 0, 0, 0],
+    [1, 1, 0, 0],
+    [0, 0, 0, 1],
+    [0, 1, 0, 0]
+]
+print(dfs(matrix, 0, 0, set()))
